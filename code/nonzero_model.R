e70159fd9ddb5364d225d50c1f5b987e86d1c6e7
sahirbhatnagar <sahir.bhatnagar@gmail.com>
2014-03-14 01:09:21 -0400

    code for non_zero model (respondents with non-zero ty time use)

---
+++ code/nonzero_model.R
@@ -0,0 +1,13 @@
+##################################
+# R source code file used to model the ATUS respondents with non-zero tv time use
+# Created by Sahir, March 14, 2014
+# Updated March 14, 2014
+# hosted on Github repo 'sahirbhatnagar/atus'
+# NOTE: 
+##################################
+
+# directory where the data and list of variables are located
+setwd("~/git_repositories/atus.git/data")
+
+# import the data
+source("~/git_repositories/atus.git/code/prelim_fit.R")
