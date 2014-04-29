c5cc268babe70819349fe101145b6c33953a2c33
kevisco <kevisco@gmail.com>
2014-04-29 12:43:00 -0400

    final economy data structure is variable ECON

--- code/pca.R
+++ code/pca.R
@@ -6,7 +6,7 @@
 # NOTE:
 ##################################
 
-setwd("~/git_repositories/atus.git/data")
+setwd("~/atus/data")
 
 #Initial data read
 gdp_data = read.table("real_gdp_data.txt", header=TRUE)
@@ -25,10 +25,62 @@ for (i in seq(1, length(dow_data$AdjClose), by = 3)) {
   count = count + 1
 }
 
+#Looking at employment instead of unemployment
+employment_quart = 100-unemployment_quart
+
 #Merging into single data matrix in order to do PCA
-econ_measures = cbind(gdp_quart, dow_quart, sp500_quart, unemployment_quart)
+econ_measures = cbind(gdp_quart, dow_quart, sp500_quart, employment_quart)
 
 #Trying PCA. Setting parameter cor=TRUE as vectors have different units.
 pca_econ = princomp(econ_measures, cor=TRUE)
 summary(pca_econ)
 pca_econ$loadings
+
+#Scaling down and centering the different measures for simultaneous plot
+gdp_scale = (gdp_quart-pca_econ$center[1])/pca_econ$scale[1]
+dow_scale = (dow_quart-pca_econ$center[2])/pca_econ$scale[2]
+sp500_scale = (sp500_quart-pca_econ$center[3])/pca_econ$scale[3]
+employment_scale = (employment_quart-pca_econ$center[4])/pca_econ$scale[4]
+
+#plotting
+plot(-50, -50, xlim=c(0,length(gdp_scale)), ylim=c(min(gdp_scale),max(gdp_scale)))
+lines(1:length(gdp_scale), gdp_scale, col='red')
+lines(1:length(dow_scale), dow_scale, col='blue')
+lines(1:length(sp500_scale), sp500_scale, col='green')
+lines(1:length(employment_scale), employment_scale, col='purple')
+
+#looking at negative of pca scores to better compare to data
+lines(1:length(gdp_scale), -pca_econ$scores[,1], col=6, lty=2)
+lines(1:length(gdp_scale), -pca_econ$scores[,2], col=9, lty=2)
+
+
+
+#Final data structure for economy and time by month
+ECON = cbind(-pca_econ$scores[,1], -pca_econ$scores[,2], 1:length(pca_econ$scores[,1]))
+
+
+
+
+
+
+
+#Trying PCA without SP500 vector
+#econ_reduce = cbind(gdp_quart, dow_quart, unemployment_quart)
+#pca_reduce = princomp(econ_reduce, cor=TRUE)
+#summary(pca_reduce)
+#pca_reduce$loadings
+
+
+#plotting again... no sp500
+#plot(-50, -50, xlim=c(0,length(gdp_scale)), ylim=c(min(gdp_scale),max(gdp_scale)))
+#lines(1:length(gdp_scale), gdp_scale, col='red')
+#lines(1:length(dow_scale), dow_scale, col='blue')
+#lines(1:length(sp500_scale), sp500_scale, col='green')
+#lines(1:length(unemployment_scale), unemployment_scale, col='purple')
+
+#lines(1:length(gdp_scale), pca_reduce$scores[,1], col=8)
+#lines(1:length(gdp_scale), pca_reduce$scores[,2], col=9)
+
+
+
+
