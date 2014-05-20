1e1bfa1d10234496dd1d33f4ac4d3f05e51bccee
sahirbhatnagar <sahir.bhatnagar@gmail.com>
2014-05-19 22:03:39 -0400

    updated for sex econ interaction

--- code/gamma_jags_model.R
+++ code/gamma_jags_model.R
@@ -32,7 +32,7 @@ xtabs(~DT$TUYEAR)
 DT <- DT[TVIND!=0]
 #Sample subset of survey data
 set.seed(1234)
-prop <- 0.10
+prop <- 0.01
 DTS <- DT[sample(x=1:nrow(DT), size=prop*nrow(DT)), , ]
 xtabs(~DTS$QUARTER)
 
@@ -131,7 +131,7 @@ load("~/Dropbox/PhD/SSC case study/gamma1_no_interaction.RData")
 save.image(file="~/Dropbox/PhD/SSC case study/gamma1_no_interaction.RData")
 getwd()
 
-mcmcChain = as.matrix( ss_econ2 )
+mcmcChain = as.matrix( ss_econ )
 chainLength = NROW(mcmcChain)
 
 source("~/git_repositories/atus/code/plotPost.R")
@@ -161,8 +161,8 @@ for ( idx in round(seq(1,chainLength,length=nPPC)) ) {
 }
 # Posterior estimates of parameters:
 par( mar=c(3,1,2.5,0) , mgp=c(2,0.7,0) )
-plotPost( mcmcChain[,"rate[100]"] , xlab="mType" , main="Posterior Est." , showMode=T )
-plotPost( mcmcChain[,"sd"] , xlab="sd" , main="Posterior Est." , showMode=T )
+plotPost( mcmcChain[,"beta_econ_1"] , xlab="mType" , main="Posterior Est." , showMode=T )
+plotPost( mcmcChain[,"beta_econ_2"] , xlab="sd" , main="Posterior Est." , showMode=T )
 savePlot(file=paste(fileNameRoot,mType,sep=""),type="jpg")
 
 xComb=seq(min(y),max(y),length=501)
@@ -256,19 +256,17 @@ abline(h=0,col="red", pch=10)
 save.image(file="gamma_model_2.RData")
 
 
-
-
-
-#remember to do DIC, age interaction, difference of betas
-
 # Sex Economy interaction -------------------------------------------------
 
+model <- jags.model(file.path(inpath, 'gamma_econ_sex_model'), data=datalist.gam, 
+                    n.chains=2, n.adapt=3000, quiet=FALSE)
 
-model <- jags.model(file.path(inpath, 'testing'), data=datalist.gam, 
-                    n.chains=2, n.adapt=5000, quiet=FALSE)
+ss_econ = coda.samples(model, c("beta_econ_1", "beta_econ_2", "logRR_time","beta_econ_1_sex",
+                                "beta_econ_2_sex","P.res","P.res.new","C.res","resid","fit",
+                                "fit.new","chisqp"), 10000, thin=10)
 
-ss_econ = coda.samples(model, c("beta_econ_1", "beta_econ_2", "gamma", "logRR_time","beta_econ_1_sex",
-                                "beta_econ_2_sex","RESPONSE"), 5000, thin=10)
+
+
 save.image(file="~/Dropbox/PhD/SSC case study/gamma2_interaction.RData")
 
 
@@ -301,13 +299,122 @@ abline(h=0,col="red", pch=10)
 
 # Econ Race interaction --------------------------------------------------------
 
+load("~/Dropbox/PhD/SSC case study/gamma3_race_interaction.RData")
+
 model <- jags.model(file.path(inpath, 'testing'), data=datalist.gam, 
-                    n.chains=2, n.adapt=7500, quiet=FALSE)
+                    n.chains=2, n.adapt=1000, quiet=FALSE)
 
 ss_econ = coda.samples(model, c("beta_econ_1", "beta_econ_2", "logRR_time","beta_econ_1_race",
-                                "beta_econ_2_race","P.res","fit","mu"), 7500, thin=10)
+                                "beta_econ_2_race","P.res","fit","mu"), 1000, thin=10)
+
+plot(ss_econ, ask=TRUE)
+
+
+
 save.image(file="~/Dropbox/PhD/SSC case study/gamma3_race_interaction.RData")
 
 summary(ss_econ)
 
 xtabs(~DTS$PTDTRACE)
+
+#econ 1
+mean(ss_econ[[1]][,10942])
+#econ 1 race interaction
+mean(ss_econ[[1]][,10943])
+
+ss_econ[[1]][,"mu[1]":"mu[10941]"]
+
+fitted <- mcmcChain[,11075:22015]
+mean.fitted <- apply(fitted,2,mean)
+
+plot(mean.fitted,DTS$TVTIME)
+dev.off()
+
+plot(model$mean$P.res, las=1, col="blue", pch=20)
+abline(h=0)
+
+
+
+# Observed vs expected -------------------------------------------------------
+
+
+summary(ss_econ)
+mcmcChain <- as.matrix(ss_econ)
+
+head(mcmcChain[,1097:1098])
+head(mcmcChain)
+fitted <- mcmcChain[,1219:2312]
+mean.fitted <- apply(fitted,2,mean)
+
+
+plot(mean.fitted,DTS$TVTIME)
+
+
+plot(mcmcChain[,"fit"], mcmcChain[,"fit.new"], main="Posterior predictive check \nfor sum of squared Pearson residuals", xlab="Discrepancy measure for actual data set", ylab="Discrepancy measure for perfect data sets", col="blue")
+abline(0, 1, lwd=2, col = "black")
+
+mean(mcmcChain[,"fit.new"]>mcmcChain[,"fit"])
+hist(mcmcChain[,"fit"])
+
+
+sim <- apply(mcmcChain[,1:1094],2,mean)
+
+
+
+plot(pred.m$value,DTS$TVTIME)
+plot(density(DTS$TVTIME))
+lines(density(pred.m$value))
+
+head(mcmcChain[,1097:1216])
+
+boxplot(mcmcChain[,1097:1216],use.cols=TRUE, 
+        main=expression(paste(hat(gamma)[month_year], " for month,year")),xaxt="n")
+
+plot(ss_econ, ask=TRUE)
+
+
+
+
+# Final Plots -------------------------------------------------------------
+
+model <- jags.model(file.path(inpath, 'testing'), data=datalist.gam, 
+                    n.chains=2, n.adapt=1000, quiet=FALSE)
+
+ss_econ = coda.samples(model, c("chisq.p"), 1000, thin=10)
+
+summary(ss_econ)
+mcmcChain <- as.matrix(ss_econ)
+head(mcmcChain[,1:5])
+
+
+fitted <- mcmcChain[,1:1094]
+residuals <- mcmcChain[,1095:2188]
+fitted.m <- melt(fitted)
+residuals.m <- melt(residuals)
+
+plot(apply(fitted,2,mean),apply(residuals,2,mean))
+hist(residuals.m$value)
+
+
+
+
+
+
+mean.resid <- apply(pred,2,mean)
+plot(DTS$ECON1,mean.resid)
+plot(DTS$ECON2,mean.resid)
+abline(h=0,col="red", pch=10)
+
+
+# Observed vs fitted values -----------------------------------------------
+
+library(reshape2)
+library(ggplot2)
+
+pred <- mcmcChain[,1:1094]
+pred.m <- melt(pred)
+
+m <- ggplot(DTS, aes(x=TVTIME))
+m + geom_histogram(binwidth=25)+aes(y=..density..) + 
+  geom_density(data=pred.m, aes(x=value), size=2)
+
